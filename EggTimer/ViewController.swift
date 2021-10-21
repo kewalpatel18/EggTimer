a8168cb8e38fccb0475e090cdd638fc8e435195d
Kewal Patel <kaakaa135@gmail.com>
2021-10-20 21:10:03 -0400

    class exit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -36,6 +36,8 @@ class ViewController: UIViewController {
         // output of hardness = hard
         print(hardness)
         
+        outputLabel.text = hardness
+        
         totalTime = eggTimes[hardness]!   //10 | 7 | 4
         
         Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
